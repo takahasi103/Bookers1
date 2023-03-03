933a4f8fe18395f95af233084086a54c761bd6b3
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-03-03 12:37:00 +0000

    [Start] bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  def index
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.save
+  end 
+
+  def show
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+end
