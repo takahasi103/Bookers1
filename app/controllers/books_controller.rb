c607110a271e048e46391e99f2fec8d216da9874
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-03-03 13:09:49 +0000

    [Save]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,18 @@
 class BooksController < ApplicationController
+  
   def index
+    @books = Book.all.order(create_at: :asc)
+    @book = Book.new
   end
   
   def create
     @book = Book.new(book_params)
     @book.save
+    redirect_to book_path(@book.id)
   end 
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -22,6 +27,6 @@ class BooksController < ApplicationController
   private
   
   def book_params
-    params.require(:book).permit(:title,:body)
+    params.require(:book).permit(:title, :body)
   end
 end
